77694a1e10fbe202e96fb2e96d0961733693d4b6
RiZeFlame <47260986+RiZeFlame@users.noreply.github.com>
2021-04-20 23:12:04 -0700

    Working commit

--- src/main/java/me/akiradev/oasiscapes/client/OasiscapesClient.java
+++ src/main/java/me/akiradev/oasiscapes/client/OasiscapesClient.java
@@ -1,35 +1,35 @@
 package me.akiradev.oasiscapes.client;
 
-import me.akiradev.oasiscapes.events.PostTickEvent;
 import me.zero.alpine.bus.EventBus;
 import me.zero.alpine.bus.EventManager;
-import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listenable;
-import me.zero.alpine.listener.Listener;
 import me.akiradev.oasiscapes.Utils.Capes;
 import me.akiradev.oasiscapes.Utils.Executor;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class OasiscapesClient implements ClientModInitializer, Listenable {
     public static OasiscapesClient INSTANCE;
     public static final EventBus EVENT_BUS = new EventManager();
+    private static int tick;
     @Override
     public void onInitializeClient() {
-        if (INSTANCE == null) {
-            INSTANCE = this;
-            return;
-        }
+
+        System.out.println("Initializing Oasis Capes.");
+
         Executor.init();
         Capes.init();
-        EVENT_BUS.subscribe(this);
-    }
-    @EventHandler
-    private final Listener<PostTickEvent> onTick = new Listener<>(event -> {
-        Capes.tick();
-    });
 
-    public static <T> T postEvent(T event) {
-        EVENT_BUS.post(event);
-        return event;
+        EVENT_BUS.subscribe(this);
+
+        ClientTickEvents.START_CLIENT_TICK.register((player) -> {
+            if(tick == 20){
+                Capes.tick();
+                tick = 0;
+            }else{
+                tick++;
+            }
+        });
+
     }
 }
